8997398c6542194a9dadb1230ee24ff2c1d5870d
taba <hgKaguyaToupako4er@gmail.com>
2018-02-09 21:58:28 +0900

    first commit

---
+++ app/src/main/java/com/example/ykonomi/dominion_randomizer/SelectActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ykonomi.dominion_randomizer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select)
+
+
+
+    }
+}
